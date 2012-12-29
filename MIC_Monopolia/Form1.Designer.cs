51e8f217c1c0bdee703e16cd4c3051221fddebe0
kalashnikovisme <kalashnikovisme@gmail.com>
2012-12-29 22:42:17 +0400

    initialize commit

---
+++ MIC_Monopolia/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MIC_Monopolia {
+	partial class MainField {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.fieldTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.playSpaceTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+			this.fieldTableLayoutPanel.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// fieldTableLayoutPanel
+			// 
+			this.fieldTableLayoutPanel.ColumnCount = 2;
+			this.fieldTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.58491F));
+			this.fieldTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.41509F));
+			this.fieldTableLayoutPanel.Controls.Add(this.playSpaceTableLayoutPanel, 0, 0);
+			this.fieldTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.fieldTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+			this.fieldTableLayoutPanel.Name = "fieldTableLayoutPanel";
+			this.fieldTableLayoutPanel.RowCount = 1;
+			this.fieldTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.fieldTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.fieldTableLayoutPanel.Size = new System.Drawing.Size(689, 418);
+			this.fieldTableLayoutPanel.TabIndex = 0;
+			// 
+			// playSpaceTableLayoutPanel
+			// 
+			this.playSpaceTableLayoutPanel.ColumnCount = 10;
+			this.playSpaceTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.playSpaceTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.playSpaceTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.playSpaceTableLayoutPanel.Location = new System.Drawing.Point(3, 3);
+			this.playSpaceTableLayoutPanel.Name = "playSpaceTableLayoutPanel";
+			this.playSpaceTableLayoutPanel.RowCount = 10;
+			this.playSpaceTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.playSpaceTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.playSpaceTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.playSpaceTableLayoutPanel.Size = new System.Drawing.Size(501, 412);
+			this.playSpaceTableLayoutPanel.TabIndex = 0;
+			// 
+			// MainField
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(689, 418);
+			this.Controls.Add(this.fieldTableLayoutPanel);
+			this.Name = "MainField";
+			this.Text = "MainField";
+			this.Load += new System.EventHandler(this.MainField_Load);
+			this.fieldTableLayoutPanel.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel fieldTableLayoutPanel;
+		private System.Windows.Forms.TableLayoutPanel playSpaceTableLayoutPanel;
+	}
+}
+
---
+++ MIC_Monopolia/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MIC_Monopolia {
+	public partial class MainField : Form {
+		private const int CELL_X = 10;
+		private const int CELL_Y = 10;
+		private const int PERCENT_100 = 100;
+		
+		public MainField() {
+			InitializeComponent();
+			this.Font = new Font("PF Beausans Pro Light", 12F);
+			fieldTableLayoutPanel.ColumnStyles.Insert(0, new ColumnStyle(SizeType.Percent, 80));
+			fieldTableLayoutPanel.RowCount = 1;
+			playSpaceTableLayoutPanel.ColumnCount = CELL_X;
+			playSpaceTableLayoutPanel.RowCount = CELL_Y;
+			for (int i = 0; i < playSpaceTableLayoutPanel.ColumnCount; i++) {
+				playSpaceTableLayoutPanel.ColumnStyles.Insert(i, new ColumnStyle(SizeType.Percent, PERCENT_100 / playSpaceTableLayoutPanel.ColumnCount));
+			}
+			for (int i = 0; i < playSpaceTableLayoutPanel.RowCount; i++) {
+				playSpaceTableLayoutPanel.RowStyles.Insert(i, new RowStyle(SizeType.Percent, PERCENT_100 / playSpaceTableLayoutPanel.RowCount));
+			}
+			
+			playSpaceTableLayoutPanel.CellBorderStyle = TableLayoutPanelCellBorderStyle.InsetDouble;
+		}
+
+		private void MainField_Load(object sender, EventArgs e) {
+
+		}
+	}
+}
